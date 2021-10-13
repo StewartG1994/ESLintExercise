edd41a3e247e07d2965183e9a16bddc349cac142
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-10-13 17:31:16 +0100

    exercise complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('test');
+import './styles.css';
+
+const navBtn = document.querySelector('.navBtn');
+const navDropDown = document.querySelector('.dropDown');
+
+function displayMenu() {
+  navBtn.addEventListener('click', () => {
+    navDropDown.classList.toggle('display');
+  });
+}
+
+displayMenu();
